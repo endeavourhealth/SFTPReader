7de8d12ea9f2dc5538321befbfd6f6226eeb4d0e
Jonny Rylands <jonny.rylands@vitalsignssoftware.com>
2017-02-16 09:57:46 +0000

    Initial checkin (migration from EDS repo)

---
+++ deploy/sftpreader.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export JAVA_HOME=/usr/lib/jvm/java-8-oracle/jre
+export CONFIG_JDBC_USERNAME=postgres
+export CONFIG_JDBC_PASSWORD=
+export CONFIG_JDBC_URL=jdbc:postgresql://127.0.0.1/config
+
+java -jar -DINSTANCE_NAME=TEST001  /opt/sftpreader/eds-sftpreader-1.0-SNAPSHOT.jar -Xmx4g
