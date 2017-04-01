29d5572f40af1a00cecf076fd0b083fc69c6ccbd
Jonny Rylands <jonny.rylands@vitalsignssoftware.com>
2017-04-02 00:45:45 +0100

    Add max heap memory allocation

--- deploy/run.sh
+++ deploy/run.sh
@@ -5,4 +5,4 @@ export CONFIG_JDBC_USERNAME=postgres
 export CONFIG_JDBC_PASSWORD=
 export CONFIG_JDBC_URL=jdbc:postgresql://127.0.0.1/config
 
-java -jar -DINSTANCE_NAME=TEST001  /opt/sftpreader/sftpreader-1.0-SNAPSHOT.jar -Xmx4g
+java -Xmx4g -jar -DINSTANCE_NAME=TEST001  /opt/sftpreader/sftpreader-1.0-SNAPSHOT.jar
