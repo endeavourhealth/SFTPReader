a1dd8e8851bba22887c7cc9b3c35feadafd42184
Jonny Rylands <jonny.rylands@vitalsignssoftware.com>
2017-04-26 09:38:35 +0100

    Tidy EmisSftpNotificationCreator

--- src/sftpreader/src/main/java/org/endeavourhealth/sftpreader/implementations/emis/EmisSftpNotificationCreator.java
+++ src/sftpreader/src/main/java/org/endeavourhealth/sftpreader/implementations/emis/EmisSftpNotificationCreator.java
@@ -1,60 +1,41 @@
 package org.endeavourhealth.sftpreader.implementations.emis;
 
 import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.endeavourhealth.sftpreader.implementations.SftpNotificationCreator;
-import org.endeavourhealth.sftpreader.model.db.Batch;
-import org.endeavourhealth.sftpreader.model.db.BatchFile;
 import org.endeavourhealth.sftpreader.model.db.BatchSplit;
 import org.endeavourhealth.sftpreader.model.db.DbConfiguration;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class EmisSftpNotificationCreator extends SftpNotificationCreator {
 
     @Override
     public String createNotificationMessage(DbConfiguration dbConfiguration, BatchSplit batchSplit) {
 
-        Batch batch = batchSplit.getBatch();
+        String combinedPath = FilenameUtils.concat(dbConfiguration.getLocalRootPath(), batchSplit.getLocalRelativePath());
 
-        //sort batches by filename, so it's consistently ordered
-        List<BatchFile> batchFiles = batch
-                .getBatchFiles()
-                .stream()
-                .sorted(Comparator.comparing(t -> t.getFilename()))
-                .collect(Collectors.toList());
+        List<String> files = findFiles(new File(combinedPath), batchSplit.getLocalRelativePath());
 
-        String relativePath = batchSplit.getLocalRelativePath();
-        String rootPath = dbConfiguration.getLocalRootPath();
-        String combinedPath = FilenameUtils.concat(rootPath, relativePath);
-        File combinedFile = new File(combinedPath);
-
-        List<String> files = new ArrayList<>();
-        findFiles(files, combinedFile, relativePath);
-
-        StringBuilder sb = new StringBuilder();
-
-        for (String file: files) {
-            sb.append(file);
-            sb.append(System.lineSeparator());
-        }
-
-        return sb.toString();
+        return StringUtils.join(files, "\r\n");
     }
 
-    private static void findFiles(List<String> files, File dir, String relativePath) {
+    private static List<String> findFiles(File dir, String relativePath) {
+
+        List<String> result = new ArrayList<>();
 
         for (File f: dir.listFiles()) {
             String newRelativePath = FilenameUtils.concat(relativePath, f.getName());
 
-            if (f.isDirectory()) {
-                findFiles(files, f, newRelativePath);
-            } else {
-                files.add(newRelativePath);
-            }
+            if (f.isDirectory())
+                result.addAll(findFiles(f, newRelativePath));
+            else
+                result.add(newRelativePath);
+
         }
+
+        return result;
     }
 }
