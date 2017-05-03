65cc4d22b429f4d0eb9244cf8a3407211ccc687d
Jonny Rylands <jonny.rylands@vitalsignssoftware.com>
2017-05-04 00:14:26 +0100

    Fix incorrectly defined constraint

--- src/sftpreader-database/src/main/schema/V1.5-schema.sql
+++ src/sftpreader-database/src/main/schema/V1.5-schema.sql
@@ -2,7 +2,7 @@
 	Schema V1.5: Rename instance_id columns to configuration_id
 */
 
--- drop constraint configuration_configuration_instanceid_pk and dependents
+-- 1a. drop constraint configuration_configuration_instanceid_pk and dependents
 alter table log.unknown_file drop constraint log_unknownfile_instanceid_fk;
 alter table configuration.configuration_sftp drop constraint configuration_configurationsftp_instanceid_fk;
 alter table configuration.configuration_sftp drop constraint configuration_configurationsftp_instanceid_pk;
@@ -12,25 +12,25 @@ alter table configuration.configuration_kvp drop constraint configuration_config
 alter table configuration.configuration_kvp drop constraint configuration_configuration_instanceid_fk;
 alter table configuration.configuration drop constraint configuration_configuration_instanceid_pk;
 
--- drop constraint configuration_configuration_instanceid_interfacetypeid_uq and dependents
+-- 1b. drop constraint configuration_configuration_instanceid_interfacetypeid_uq and dependents
 alter table log.batch drop constraint log_batch_instanceid_interfacetypeid_fk;
 alter table configuration.configuration drop constraint configuration_configuration_instanceid_interfacetypeid_uq;
 
--- drop constraint log_batch_instanceid_batchid_uq and dependents
+-- 1c. drop constraint log_batch_instanceid_batchid_uq and dependents
 alter table log.batch_split drop constraint log_batchsplit_instanceid_interfacetypeid_fk;
 alter table log.batch drop constraint log_batch_instanceid_batchid_uq;
 
--- drop constraint log_batchsplit_instanceid_batchid_uq and dependents
+-- 1d. drop constraint log_batchsplit_instanceid_batchid_uq and dependents
 alter table log.notification_message drop constraint log_notificationmessage_batchid_instanceid_fk;
 alter table log.batch_split drop constraint log_batchsplit_instanceid_batchid_uq;
 
--- drop constraint log_batch_instanceid_batchidentifier_uq
+-- 1e. drop constraint log_batch_instanceid_batchidentifier_uq
 alter table log.batch drop constraint log_batch_instanceid_batchidentifier_uq;
 
--- drop constraint log_batch_instanceid_sequencenumber_uq
+-- 1f. drop constraint log_batch_instanceid_sequencenumber_uq
 alter table log.batch drop constraint log_batch_instanceid_sequencenumber_uq;
 
--- rename instance_id columns to configuration_id
+-- 2. rename instance_id columns to configuration_id
 alter table configuration.configuration rename instance_id to configuration_id;
 alter table configuration.configuration rename local_instance_path_prefix to local_root_path_prefix;
 alter table configuration.configuration rename local_instance_path to local_root_path;
@@ -43,7 +43,7 @@ alter table log.batch_split rename instance_id to configuration_id;
 alter table log.notification_message rename instance_id to configuration_id;
 alter table log.unknown_file rename instance_id to configuration_id;
 
--- recreate constraint configuration_configuration_configurationid_pk and dependents
+-- 3a. recreate constraint configuration_configuration_configurationid_pk and dependents
 alter table configuration.configuration add constraint configuration_configuration_configurationid_pk primary key (configuration_id);
 alter table configuration.configuration_sftp add constraint configuration_configurationsftp_configurationid_pk primary key (configuration_id);
 alter table configuration.configuration_sftp add constraint configuration_configurationsftp_configurationid_fk foreign key (configuration_id) references configuration.configuration(configuration_id);
@@ -53,25 +53,25 @@ alter table configuration.configuration_kvp add constraint configuration_configu
 alter table configuration.configuration_kvp add constraint configuration_configurationkvp_configurationid_fk foreign key (configuration_id) references configuration.configuration(configuration_id);
 alter table log.unknown_file add constraint log_unknownfile_configurationid_fk foreign key (configuration_id) references configuration.configuration(configuration_id);
 
--- recreate constraint log_batch_configurationid_interfacetypeid_fk and dependents
-alter table log.batch add constraint log_batch_configurationid_interfacetypeid_fk unique (configuration_id, interface_type_id);
+-- 3b. recreate constraint configuration_configuration_configurationid_interfacetypeid_uq and dependents
 alter table configuration.configuration add constraint configuration_configuration_configurationid_interfacetypeid_uq unique (configuration_id, interface_type_id);
+alter table log.batch add constraint log_batch_configurationid_interfacetypeid_fk foreign key (configuration_id, interface_type_id) references configuration.configuration (configuration_id, interface_type_id);
 
--- recreate constraint log_batch_configurationid_batchid_uq and dependents
+-- 3c. recreate constraint log_batch_configurationid_batchid_uq and dependents
 alter table log.batch add constraint log_batch_configurationid_batchid_uq unique (configuration_id, batch_id);
 alter table log.batch_split add constraint log_batchsplit_configurationid_batchid_fk foreign key (configuration_id, batch_id) references log.batch (configuration_id, batch_id);
 
--- recreate constraint log_batchsplit_configurationid_batchid_batchsplitid_uq and dependents
+-- 3d. recreate constraint log_batchsplit_configurationid_batchid_batchsplitid_uq and dependents
 alter table log.batch_split add constraint log_batchsplit_configurationid_batchid_batchsplitid_uq unique (configuration_id, batch_id, batch_split_id);
 alter table log.notification_message add constraint log_notificationmessage_configurationid_batchid_batchsplitid_fk foreign key (configuration_id, batch_id, batch_split_id) references log.batch_split(configuration_id, batch_id, batch_split_id);
 
--- recreate constraint log_batch_configurationid_batchidentifier_uq
+-- 3e. recreate constraint log_batch_configurationid_batchidentifier_uq
 alter table log.batch add constraint log_batch_configurationid_batchidentifier_uq unique (configuration_id, batch_identifier);
 
--- recreate constraint log_batch_configurationid_sequencenumber_uq
+-- 3f. recreate constraint log_batch_configurationid_sequencenumber_uq
 alter table log.batch add constraint log_batch_configurationid_sequencenumber_uq unique (configuration_id, sequence_number);
 
--- rename remaining constraints
+-- 4. rename remaining constraints
 alter table configuration.configuration rename constraint configuration_configuration_localinstancepaths_uq to configuration_configuration_localrootpaths_uq;
 alter table configuration.configuration rename constraint configuration_configuration_localinstancepaths_ck to configuration_configuration_localrootpaths_ck;
 alter table configuration.configuration rename constraint configuration_configuration_instancefriendlyname_uq to configuration_configuration_configurationfriendlyname_uq;
