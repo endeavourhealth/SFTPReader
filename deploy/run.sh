31607eaf2f6f64e12614baef54af2e2990869ef0
Jonny Rylands <jonny.rylands@vitalsignssoftware.com>
2017-05-06 10:55:42 +0100

    Update deployment scripts

--- deploy/run.sh
+++ deploy/run.sh
@@ -5,4 +5,4 @@ export CONFIG_JDBC_USERNAME=postgres
 export CONFIG_JDBC_PASSWORD=
 export CONFIG_JDBC_URL=jdbc:postgresql://127.0.0.1/config
 
-java -Xmx4g -jar -DINSTANCE_NAMES=TEST001  /opt/sftpreader/sftpreader-1.0-SNAPSHOT.jar
+java -Xmx4g -jar -DINSTANCE_NAME=TEST /opt/sftpreader/sftpreader-1.0-SNAPSHOT.jar
