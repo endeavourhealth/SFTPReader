908b077ba8256b16b43825ae4a7eebba5acce140
Jonny Rylands <jonny.rylands@vitalsignssoftware.com>
2017-05-22 23:43:39 +0100

    Add REST support to management interface

--- src/sftpreader/src/main/java/org/endeavourhealth/sftpreader/management/ManagementService.java
+++ src/sftpreader/src/main/java/org/endeavourhealth/sftpreader/management/ManagementService.java
@@ -1,10 +1,14 @@
 package org.endeavourhealth.sftpreader.management;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.webapp.WebAppContext;
 import org.endeavourhealth.sftpreader.Configuration;
+import org.endeavourhealth.sftpreader.management.rest.ManagementRestEndpoint;
 import org.slf4j.LoggerFactory;
 
+import java.net.URISyntaxException;
 import java.net.URL;
 
 public class ManagementService {
@@ -32,19 +36,44 @@ public class ManagementService {
         LOG.info("Starting http management interface on port " + httpPort.toString());
 
         this.server = new Server(httpPort.intValue());
-        WebAppContext root = new WebAppContext();
-        root.setContextPath("/");
-        root.setDescriptor(WEBAPP_RESOURCES_LOCATION + "/WEB-INF/web.xml");
+
+        WebAppContext webAppContext = createWebAppContext();
+        ServletContextHandler restServletContext = createRestServletContext();
+
+        this.server.setHandler(webAppContext);
+
+        //this.server.setHandler(restServletContext);
+        this.server.start();
+    }
+
+    private WebAppContext createWebAppContext() throws URISyntaxException {
+        WebAppContext context = new WebAppContext();
+        context.setContextPath("/");
+        context.setDescriptor(WEBAPP_RESOURCES_LOCATION + "/WEB-INF/web.xml");
 
         URL webAppDir = Thread.currentThread().getContextClassLoader().getResource(WEBAPP_RESOURCES_LOCATION);
+
         if (webAppDir == null)
             throw new RuntimeException(String.format("No %s directory was found into the JAR file", WEBAPP_RESOURCES_LOCATION));
 
-        root.setResourceBase(webAppDir.toURI().toString());
-        root.setParentLoaderPriority(true);
+        context.setResourceBase(webAppDir.toURI().toString());
+        context.setParentLoaderPriority(true);
 
-        this.server.setHandler(root);
-        this.server.start();
+        return context;
+    }
+
+    private ServletContextHandler createRestServletContext() {
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/api");
+
+        ServletHolder jerseyServlet = context.addServlet(org.glassfish.jersey.servlet.ServletContainer.class, "/*");
+        jerseyServlet.setInitOrder(0);
+
+        jerseyServlet.setInitParameter(
+                "jersey.config.server.provider.classnames",
+                ManagementRestEndpoint.class.getCanonicalName());
+
+        return context;
     }
 
     public void stop() throws Exception {
