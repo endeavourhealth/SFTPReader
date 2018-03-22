659e2983c9d592a5977eb6a00442cfa7a4ba056c
Drew Littler <drewlittler@hotmail.com>
2018-03-22 12:58:12 +0000

    Removed file combining for Barts data and added consistent ordering to Exchange payload

--- src/sftpreader/src/main/java/org/endeavourhealth/sftpreader/implementations/barts/BartsSftpBatchSplitter.java
+++ src/sftpreader/src/main/java/org/endeavourhealth/sftpreader/implementations/barts/BartsSftpBatchSplitter.java
@@ -19,7 +19,23 @@ import java.util.*;
 public class BartsSftpBatchSplitter extends SftpBatchSplitter {
     private static final Logger LOG = LoggerFactory.getLogger(BartsSftpBatchSplitter.class);
 
-    public static final String COMBINED = "COMBINED";
+    @Override
+    public List<BatchSplit> splitBatch(Batch batch, DataLayer db, DbInstanceEds instanceConfiguration, DbConfiguration dbConfiguration) throws Exception {
+
+        //create a single batch split and return it
+        List<BatchSplit> ret = new ArrayList<>();
+
+        BatchSplit batchSplit = new BatchSplit();
+        batchSplit.setBatchId(batch.getBatchId());
+        batchSplit.setLocalRelativePath(batch.getLocalRelativePath());
+        batchSplit.setOrganisationId("R1H");
+
+        ret.add(batchSplit);
+
+        return ret;
+    }
+
+    /*public static final String COMBINED = "COMBINED";
 
     private static CSVFormat CSV_FORMAT = CSVFormat.DEFAULT
                                 .withDelimiter('|')
@@ -29,9 +45,11 @@ public class BartsSftpBatchSplitter extends SftpBatchSplitter {
                                 .withQuote((Character)null)
                                 .withQuoteMode(QuoteMode.NONE);
 
-    /**
+
+
+    *//**
      * Confusingly enough, we actually COMBINE separate barts files
-     */
+     *//*
     @Override
     public List<BatchSplit> splitBatch(Batch batch, DataLayer db, DbInstanceEds instanceConfiguration, DbConfiguration dbConfiguration) throws Exception {
 
@@ -199,76 +217,6 @@ public class BartsSftpBatchSplitter extends SftpBatchSplitter {
             //delete source files from temp
             for (File sourceFile: sourceFiles) {
                 sourceFile.delete();
-            }
-        }
-    }
-
-
-    /*private void combineFiles(String combinedName, List<String> filesToCombine, Batch batch, DbInstanceEds instanceConfiguration, DbConfiguration dbConfiguration) throws Exception {
-
-        LOG.debug("Combining " + filesToCombine.size() + " -> " + combinedName);
-        String sharedStorageDir = instanceConfiguration.getSharedStoragePath();
-        String tempDir = instanceConfiguration.getTempDirectory();
-        String configurationDir = dbConfiguration.getLocalRootPath();
-        String batchDir = batch.getLocalRelativePath();
-
-        String sourceTempDir = FilenameUtils.concat(tempDir, configurationDir);
-        sourceTempDir = FilenameUtils.concat(sourceTempDir, batchDir);
-
-        String sourcePermDir = FilenameUtils.concat(sharedStorageDir, configurationDir);
-        sourcePermDir = FilenameUtils.concat(sourcePermDir, batchDir);
-
-        //check if the combined file has already been processed
-        File permDestCombinedFile = new File(sourcePermDir, combinedName);
-        boolean permDestCombinedFileExists = permDestCombinedFile.exists();
-        if (permDestCombinedFileExists) {
-            LOG.debug("Combined file " + combinedName + " already exists in permanent storage...skipping");
-            return;
-        }
-
-        List<File> sourceFiles = new ArrayList<>();
-        List<File> permSourceFiles = new ArrayList<>();
-        for (String fileToCombine: filesToCombine) {
-
-            //the raw files will need to be copied from S3 to our temp directory
-            String permanentSourceFile = FilenameUtils.concat(sourcePermDir, fileToCombine);
-            File permSourceFile = new File (permanentSourceFile);
-            permSourceFiles.add(permSourceFile);
-            File tempSourceFile = new File(sourceTempDir, fileToCombine);
-
-            InputStream inputStream = FileHelper.readFileFromSharedStorage(permanentSourceFile);
-            try {
-                Files.copy(inputStream, tempSourceFile.toPath());
-            } finally {
-                inputStream.close();
-            }
-
-            sourceFiles.add(tempSourceFile);
-        }
-
-        File destinationFile = new File(sourceTempDir, combinedName);
-
-        CsvJoiner joiner = new CsvJoiner(sourceFiles, destinationFile, CSV_FORMAT);
-        joiner.go();
-
-        //copy joined file to S3 if using it
-        if (!FilenameUtils.equals(tempDir, sharedStorageDir)) {
-            LOG.trace("Copying " + combinedName + " to permanent storage");
-            String storageFilePath = FilenameUtils.concat(sourcePermDir, combinedName);
-            FileHelper.writeFileToSharedStorage(storageFilePath, destinationFile);
-
-            //delete combined file from temp
-            destinationFile.delete();
-
-            //delete source files from temp
-            for (File sourceFile: sourceFiles) {
-                sourceFile.delete();
-            }
-            //delete source files from permanent store as we have successfully combined and saved so we no longer need the parts
-            for (File permSourceFile: permSourceFiles) {
-                if (permSourceFile.exists()) {
-                    permSourceFile.delete();
-                }
             }
         }
     }*/
--- src/sftpreader/src/main/java/org/endeavourhealth/sftpreader/implementations/barts/BartsSftpNotificationCreator.java
+++ src/sftpreader/src/main/java/org/endeavourhealth/sftpreader/implementations/barts/BartsSftpNotificationCreator.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class BartsSftpNotificationCreator extends SftpNotificationCreator {
@@ -19,9 +20,90 @@ public class BartsSftpNotificationCreator extends SftpNotificationCreator {
     public String createNotificationMessage(String organisationId, DataLayer db, DbInstanceEds instanceConfiguration,
                                             DbConfiguration dbConfiguration, BatchSplit batchSplit) throws Exception {
 
-        //we have combined fragmented files into single larger ones, so need to exclude the fragments from our notification message
-        List<String> files = super.findFilesForDefaultNotificationMessage(instanceConfiguration, dbConfiguration,
-                                                                                batchSplit, null);
+        //we have multiple file fragments and occasionally get the same file type sent twice
+        //in the same day, so we need to ensure they're properly ordered, so that the queue reader
+        //can just process them in the order supplied
+        List<String> files = super.findFilesForDefaultNotificationMessage(instanceConfiguration, dbConfiguration, batchSplit, null);
+
+        //the 2.1 and 2.2 files have different naming systems, but if we simply compare on any numeric element
+        //of the file names, we get a consistent ordering that works for both
+        List<FileWrapper> fileWrappers = new ArrayList<>();
+        for (String file: files) {
+            fileWrappers.add(new FileWrapper(file));
+        }
+
+        Collections.sort(fileWrappers);
+
+        files = new ArrayList<>();
+        for (FileWrapper fileWrapper: fileWrappers) {
+            files.add(fileWrapper.getFileName());
+        }
+
+        return super.combineFilesForNotificationMessage(files);
+        //return super.createDefaultNotificationMessage(instanceConfiguration, dbConfiguration, batchSplit, null);
+    }
+
+
+    static class FileWrapper implements Comparable {
+        private String fileName = null;
+        private List<Long> numericElements = null;
+
+        public FileWrapper(String fileName) {
+            this.fileName = fileName;
+
+            //parse out any numeric elements
+            this.numericElements = new ArrayList<>();
+
+            String[] toks = fileName.split("[._-]"); //split by dot, underscore and dash
+            for (String tok: toks) {
+                try {
+                    Long l = Long.valueOf(tok);
+                    this.numericElements.add(l);
+
+                } catch (NumberFormatException nfe) {
+                    //skip it
+                }
+            }
+        }
+
+
+
+        public String getFileName() {
+            return fileName;
+        }
+
+        @Override
+        public int compareTo(Object o) {
+            if (o instanceof FileWrapper) {
+                List<Long> ourElements = this.numericElements;
+                List<Long> otherElements = ((FileWrapper)o).numericElements;
+                if (ourElements.size() > otherElements.size()) {
+                    return 1;
+
+                } else if (ourElements.size() < otherElements.size()) {
+                    return -1;
+
+                } else {
+                    for (int i=0; i<ourElements.size(); i++) {
+                        Long ourElement = ourElements.get(i);
+                        Long otherElement = otherElements.get(i);
+                        int elementComp = ourElement.compareTo(otherElement);
+                        if (elementComp != 0) {
+                            return elementComp;
+                        }
+                    }
+                }
+
+            }
+            return 0;
+        }
+    }
+
+    /*@Override
+    public String createNotificationMessage(String organisationId, DataLayer db, DbInstanceEds instanceConfiguration,
+                                            DbConfiguration dbConfiguration, BatchSplit batchSplit) throws Exception {
+
+        List<String> files = super.findFilesForDefaultNotificationMessage(instanceConfiguration, dbConfiguration, batchSplit, null);
 
         //find all the files that we combined
         List<String> combinedFilePrefixes = new ArrayList<>();
@@ -48,7 +130,6 @@ public class BartsSftpNotificationCreator extends SftpNotificationCreator {
         }
 
         return super.combineFilesForNotificationMessage(files);
-        //return super.createDefaultNotificationMessage(instanceConfiguration, dbConfiguration, batchSplit, null);
-    }
+    }*/
 
 }
