9f9a5fe09075f2aecfe01875409011061ff05982
Drew Littler <drewlittler@hotmail.com>
2018-05-30 15:44:52 +0100

    New fhir and utility repos to allow ODS lookup for Vision to use proxy settings from SlackHelper

--- src/sftpreader/src/main/java/org/endeavourhealth/sftpreader/implementations/vision/VisionSftpOrganisationHelper.java
+++ src/sftpreader/src/main/java/org/endeavourhealth/sftpreader/implementations/vision/VisionSftpOrganisationHelper.java
@@ -2,10 +2,13 @@ package org.endeavourhealth.sftpreader.implementations.vision;
 
 import org.endeavourhealth.common.ods.OdsOrganisation;
 import org.endeavourhealth.common.ods.OdsWebService;
+import org.endeavourhealth.common.utility.SlackHelper;
 import org.endeavourhealth.sftpreader.model.DataLayerI;
 
 import org.endeavourhealth.sftpreader.implementations.SftpOrganisationHelper;
 import org.slf4j.LoggerFactory;
+
+import java.net.Proxy;
 
 public class VisionSftpOrganisationHelper extends SftpOrganisationHelper {
     private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(VisionSftpOrganisationHelper.class);
@@ -14,7 +17,11 @@ public class VisionSftpOrganisationHelper extends SftpOrganisationHelper {
     public String findOrganisationNameFromOdsCode(DataLayerI db, String odsCode) {
 
         try {
-            OdsOrganisation odsOrg = OdsWebService.lookupOrganisationViaRest(odsCode);
+            //SFTP Reader instance needs a proxy to connect to the internet, so get it from the SlackHelper
+            //which has the proxy settings in its config JSON
+            Proxy proxy = SlackHelper.getProxy();
+            OdsOrganisation odsOrg = OdsWebService.lookupOrganisationViaRest(odsCode, proxy);
+            //OdsOrganisation odsOrg = OdsWebService.lookupOrganisationViaRest(odsCode);
             if (odsOrg != null) {
                 return odsOrg.getOrganisationName();
 
