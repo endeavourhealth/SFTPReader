2b46d1ee79e4cde292232201ff89cdb310610f45
Drew Littler <drewlittler@hotmail.com>
2018-07-14 10:10:04 +0100

    Support for new Barts Cener files

--- src/sftpreader/src/main/java/org/endeavourhealth/sftpreader/implementations/barts/BartsSftpFilenameParser.java
+++ src/sftpreader/src/main/java/org/endeavourhealth/sftpreader/implementations/barts/BartsSftpFilenameParser.java
@@ -18,23 +18,23 @@ public class BartsSftpFilenameParser extends SftpFilenameParser {
     private static final DateTimeFormatter BATCH_IDENTIFIER_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     //special constants for the 2.1 files, but we just use the first part of the file name for 2.2 files
-    public static final String TYPE_2_1_SUSOPA = "2.1_SUSOPA";
-    public static final String TYPE_2_1_SUSAEA = "2.1_SUSAEA";
-    public static final String TYPE_2_1_SUSIP = "2.1_SUSIP";
-    public static final String TYPE_2_1_TAILOPA = "2.1_TAILOPA";
-    public static final String TYPE_2_1_TAILAEA = "2.1_TAILAEA";
-    public static final String TYPE_2_1_TAILIP = "2.1_TAILIP";
-    public static final String TYPE_2_1_PROC = "2.1_PROC";
-    public static final String TYPE_2_1_DIAG = "2.1_DIAG";
-    public static final String TYPE_2_1_BIRTH = "2.1_BIRTH";
-    public static final String TYPE_2_1_PREG = "2.1_PREG";
-    public static final String TYPE_2_1_PROB = "2.1_PROB";
-    public static final String TYPE_2_2_SPFIT = "2.2_SPFIT"; //Surginet ???
-    public static final String TYPE_2_2_CC = "2.2_CC"; //Critial Care
-    public static final String TYPE_2_2_HDB = "2.2_HDB"; //Home Delivery and Birth
-    public static final String TYPE_2_2_FAMILY_HISTORY = "2.2_FAMILY_HISTORY";
-    public static final String TYPE_EMERGENCY_CARE = "EMERGENCY_CARE";
-    public static final String TYPE_EMERGENCY_CARE_TAILS = "EMERGENCY_CARE_TAILS";
+    public static final String TYPE_2_1_SUSOPA = "SusOutpatient";
+    public static final String TYPE_2_1_SUSAEA = "SusEmergency";
+    public static final String TYPE_2_1_SUSIP = "SusInpatient";
+    public static final String TYPE_2_1_TAILOPA = "SusOutpatientTail";
+    public static final String TYPE_2_1_TAILAEA = "SusEmergencyTail";
+    public static final String TYPE_2_1_TAILIP = "SusInpatientTail";
+    public static final String TYPE_2_1_PROC = "Procedure";
+    public static final String TYPE_2_1_DIAG = "Diagnosis";
+    public static final String TYPE_2_1_BIRTH = "Birth";
+    public static final String TYPE_2_1_PREG = "Pregnancy";
+    public static final String TYPE_2_1_PROB = "Problem";
+    public static final String TYPE_2_2_SPFIT = "SPFIT"; //Surginet ???
+    public static final String TYPE_2_2_CC = "CriticalCare"; //Critial Care
+    public static final String TYPE_2_2_HDB = "HomeDeliveryAndBirth"; //Home Delivery and Birth
+    public static final String TYPE_2_2_FAMILY_HISTORY = "FamilyHistory";
+    public static final String TYPE_EMERGENCY_CARE = "SusEmergencyCareDataSet";
+    public static final String TYPE_EMERGENCY_CARE_TAILS = "SusEmergencyCareDataSetTail";
     public static final String TYPE_APPOINTMENT_SCHEDULING = "APPSL2";
 
 
@@ -270,249 +270,5 @@ public class BartsSftpFilenameParser extends SftpFilenameParser {
         }
     }
 
-
-
-    /*protected void parseFilename(String filename, LocalDateTime lastModified) throws SftpFilenameParseException {
-        String[] parts = filename.split("_");
-
-        if (parts.length < 2)
-            throw new SftpFilenameParseException("Barts batch filename could not be parsed");
-
-        String filenamePart1 = parts[0];
-        String filenamePart2 = parts[1];
-        if (filenamePart1.compareToIgnoreCase("susopa") == 0) {
-            if (parts.length != 2)
-                throw new SftpFilenameParseException("Barts batch filename could not be parsed");
-            fileTypeIdentifier = FILE_TYPE_SUSOPA;
-            batchGroup = FILE_TYPE_SUSOPA;
-            fileUniqueId = prefixZeros(filenamePart2.substring(filenamePart2.indexOf(".") + 1));
-
-        } else if (filenamePart1.compareToIgnoreCase("susaea") == 0) {
-            if (parts.length != 2)
-                throw new SftpFilenameParseException("Barts batch filename could not be parsed");
-            fileTypeIdentifier = FILE_TYPE_SUSAEA;
-            fileUniqueId = prefixZeros(filenamePart2.substring(filenamePart2.indexOf(".") + 1));
-            batchGroup = FILE_TYPE_SUSAEA;
-
-        } else if (filenamePart1.compareToIgnoreCase("tailopa") == 0) {
-            if (parts.length != 2)
-                throw new SftpFilenameParseException("Barts batch filename could not be parsed");
-            fileTypeIdentifier = FILE_TYPE_TAILOPA;
-            fileUniqueId = prefixZeros(filenamePart2.substring(filenamePart2.indexOf(".") + 1));
-            batchGroup = FILE_TYPE_SUSOPA;
-
-        } else if (filenamePart1.compareToIgnoreCase("tailaea") == 0) {
-            if (parts.length != 2)
-                throw new SftpFilenameParseException("Barts batch filename could not be parsed");
-            fileTypeIdentifier = FILE_TYPE_TAILAEA;
-            fileUniqueId = prefixZeros(filenamePart2.substring(filenamePart2.indexOf(".") + 1));
-            batchGroup = FILE_TYPE_SUSAEA;
-
-        } else if ((filenamePart1.compareToIgnoreCase("PC") == 0) || (filenamePart1.compareToIgnoreCase("MAT") == 0) || (filenamePart1.compareToIgnoreCase("CDS") == 0)) {
-            // Bulk files
-            parseBulkFilename(filename, pgpFileExtensionFilter);
-
-
-        } else {
-            String filenamePart3 = parts[2];
-
-            if (filenamePart1.compareToIgnoreCase("tailip") == 0) {
-                if (parts.length != 3)
-                    throw new SftpFilenameParseException("Barts batch filename could not be parsed");
-                fileTypeIdentifier = FILE_TYPE_TAILIP;
-                fileUniqueId = prefixZeros(filenamePart2.split("\\.")[1]);
-                batchGroup = FILE_TYPE_SUSIP;
-            } else {
-                String filenamePart4 = parts[3];
-
-                if (filenamePart1.compareToIgnoreCase("ip") == 0) {
-                    if (parts.length != 4)
-                        throw new SftpFilenameParseException("Barts batch filename could not be parsed");
-                    fileTypeIdentifier = FILE_TYPE_SUSIP;
-                    fileUniqueId = prefixZeros(filenamePart3);
-                    batchGroup = FILE_TYPE_SUSIP;
-                } else {
-                    if (filenamePart1.compareToIgnoreCase("rnj") == 0) {
-                        if (parts.length != 4)
-                            throw new SftpFilenameParseException("Barts batch filename could not be parsed");
-                        fileTypeIdentifier = filenamePart3.toUpperCase();
-                        fileUniqueId = filenamePart4.substring(0, filenamePart4.indexOf("."));
-                        batchGroup = fileTypeIdentifier;
-                    } else {
-                        //String filenamePart5 = parts[4];
-                        String filenamePart6 = parts[5];
-
-                        if (filenamePart1.compareToIgnoreCase("GETL") == 0) {
-                            if (parts.length != 6)
-                                throw new SftpFilenameParseException("Barts batch filename could not be parsed");
-                            fileTypeIdentifier = filenamePart3.toUpperCase();
-                            fileUniqueId = filenamePart4;
-                            batchGroup = "MATERNITY";
-                        } else {
-                            throw new SftpFilenameParseException("Barts batch filename could not be parsed");
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    *//*
-     *
-     *//*
-    private void parseBulkFilename(String filename, String pgpFileExtensionFilter) throws SftpFilenameParseException {
-        String[] parts = filename.split("_");
-
-        String filenamePart1 = parts[0];
-        String filenamePart2 = parts[1];
-        String filenamePart3 = parts[2];
-        String filenamePart4 = parts[3];
-
-        if (filenamePart1.compareToIgnoreCase("MAT") == 0) {
-            // Bulk maternity files
-            if (parts.length != 4) {
-                throw new SftpFilenameParseException("Barts batch filename could not be parsed");
-            }
-            if (filenamePart2.compareToIgnoreCase("BIRTH") == 0) {
-                fileTypeIdentifier = FILE_TYPE_BULK_BIRTH;
-                fileUniqueId = filenamePart3.substring(0, 4) + ConvertMMMtoMM(filenamePart3.substring(4));
-                batchGroup = BATCH_GROUP_BULK_MATERNITY;
-            } else if (filenamePart2.compareToIgnoreCase("PREGNANCY") == 0) {
-                fileTypeIdentifier = FILE_TYPE_BULK_PREGNANCY;
-                fileUniqueId = filenamePart3.substring(0, 4) + ConvertMMMtoMM(filenamePart3.substring(4));
-                batchGroup = BATCH_GROUP_BULK_MATERNITY;
-            } else {
-                throw new SftpFilenameParseException("Barts batch filename could not be parsed");
-            }
-        } else  if (filenamePart1.compareToIgnoreCase("PC") == 0) {
-            // Bulk SNOMED files
-            if (parts.length != 4) {
-                throw new SftpFilenameParseException("Barts batch filename could not be parsed");
-            }
-            if (filenamePart2.compareToIgnoreCase("PROBLEMS") == 0) {
-                fileTypeIdentifier = FILE_TYPE_BULK_PROBLEMS;
-                fileUniqueId = filenamePart3.substring(0, 4) + ConvertMMMtoMM(filenamePart3.substring(4));
-                batchGroup = FILE_TYPE_BULK_PROBLEMS;
-            }
-            else if (filenamePart2.compareToIgnoreCase("DIAGNOSES") == 0) {
-                fileTypeIdentifier = FILE_TYPE_BULK_DIAGNOSIS;
-                fileUniqueId = filenamePart3.substring(0, 4) + ConvertMMMtoMM(filenamePart3.substring(4));
-                batchGroup = FILE_TYPE_BULK_DIAGNOSIS;
-            }
-            else if (filenamePart2.compareToIgnoreCase("PROCEDURES") == 0) {
-                fileTypeIdentifier = FILE_TYPE_BULK_PROCEDURE;
-                fileUniqueId = filenamePart3.substring(0, 4) + ConvertMMMtoMM(filenamePart3.substring(4));
-                batchGroup = FILE_TYPE_BULK_PROCEDURE;
-            }else {
-                throw new SftpFilenameParseException("Barts batch filename could not be parsed");
-            }
-
-        } else if (filenamePart1.compareToIgnoreCase("CDS") == 0) {
-            // Bulk SUS files
-            String filenamePart5 = parts[4];
-
-            if (filename.toUpperCase().startsWith("CDS_OP_ALL_TAIL_")) {
-                // Yes - file naming convention for this particular files is different
-                if (parts.length != 6) {
-                    throw new SftpFilenameParseException("Barts batch filename could not be parsed");
-                }
-                fileTypeIdentifier = FILE_TYPE_BULK_TAILOPA;
-                fileUniqueId = filenamePart5.substring(0, 4) + ConvertMMMtoMM(filenamePart5.substring(4));
-                batchGroup = FILE_TYPE_BULK_SUSOPA;
-            }
-            else if (filename.toUpperCase().startsWith("CDS_AEA_TAIL_")) {
-                fileTypeIdentifier = FILE_TYPE_BULK_TAILAEA;
-                fileUniqueId = filenamePart3.substring(0, 4) + ConvertMMMtoMM(filenamePart3.substring(4));
-                batchGroup = FILE_TYPE_BULK_SUSAEA;
-            }
-            else if (filename.toUpperCase().startsWith("CDS_APC_TAIL_")) {
-                fileTypeIdentifier = FILE_TYPE_BULK_TAILIP;
-                fileUniqueId = filenamePart3.substring(0, 4) + ConvertMMMtoMM(filenamePart3.substring(4));
-                batchGroup = FILE_TYPE_BULK_SUSIP;
-            }
-            else if (filename.toUpperCase().startsWith("CDS_OPA_")) {
-                if (parts.length != 4) {
-                    throw new SftpFilenameParseException("Barts batch filename could not be parsed");
-                }
-                fileTypeIdentifier = FILE_TYPE_BULK_SUSOPA;
-                fileUniqueId = filenamePart3.substring(0, 4) + ConvertMMMtoMM(filenamePart3.substring(4));
-                batchGroup = FILE_TYPE_BULK_SUSOPA;
-            }
-            else if (filename.toUpperCase().startsWith("CDS_AEA_")) {
-                if (parts.length != 4) {
-                    throw new SftpFilenameParseException("Barts batch filename could not be parsed");
-                }
-                fileTypeIdentifier = FILE_TYPE_BULK_SUSAEA;
-                fileUniqueId = filenamePart3.substring(0, 4) + ConvertMMMtoMM(filenamePart3.substring(4));
-                batchGroup = FILE_TYPE_BULK_SUSAEA;
-            }
-            else if (filename.toUpperCase().startsWith("CDS_APC_")) {
-                if (parts.length != 4) {
-                    throw new SftpFilenameParseException("Barts batch filename could not be parsed");
-                }
-                fileTypeIdentifier = FILE_TYPE_BULK_SUSIP;
-                fileUniqueId = filenamePart3.substring(0, 4) + ConvertMMMtoMM(filenamePart3.substring(4));
-                batchGroup = FILE_TYPE_BULK_SUSIP;
-            }
-             else
-            {
-                throw new SftpFilenameParseException("Barts batch filename could not be parsed");
-            }
-        }else {
-            throw new SftpFilenameParseException("Barts batch filename could not be parsed");
-        }
-    }
-
-    *//*
-     *
-     *//*
-    private String ConvertMMMtoMM(String shortMonth) {
-        if (shortMonth.compareToIgnoreCase("Jan") == 0) {
-            return "01";
-        }
-        else if (shortMonth.compareToIgnoreCase("Feb") == 0) {
-            return "02";
-        }
-        else if (shortMonth.compareToIgnoreCase("Mar") == 0) {
-            return "03";
-        }
-        else if (shortMonth.compareToIgnoreCase("Apr") == 0) {
-            return "04";
-        }
-        else if (shortMonth.compareToIgnoreCase("May") == 0) {
-            return "05";
-        }
-        else if (shortMonth.compareToIgnoreCase("Jun") == 0) {
-            return "06";
-        }
-        else if (shortMonth.compareToIgnoreCase("Jul") == 0) {
-            return "07";
-        }
-        else if (shortMonth.compareToIgnoreCase("Aug") == 0) {
-            return "08";
-        }
-        else if (shortMonth.compareToIgnoreCase("Sep") == 0) {
-            return "09";
-        }
-        else if (shortMonth.compareToIgnoreCase("Oct") == 0) {
-            return "10";
-        }
-        else if (shortMonth.compareToIgnoreCase("Nov") == 0) {
-            return "11";
-        }
-        else if (shortMonth.compareToIgnoreCase("Dec") == 0) {
-            return "12";
-        } else {
-            return "XX";
-        }
-    }
-
-    *//*
-     *
-     *//*
-    private String prefixZeros(String inNumber) {
-        int i = Integer.parseInt(inNumber);
-        return String.format ("%010d", i);
-    }*/
 
 }
