82c8c8efebe1309a108d736fedd3fc431b31cf7f
Drew Littler <drewlittler@hotmail.com>
2019-03-06 11:32:38 +0000

    Removed some deployment files

--- deploy/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export JAVA_HOME=/usr/lib/jvm/java-8-oracle/jre
-export CONFIG_JDBC_USERNAME=postgres
-export CONFIG_JDBC_PASSWORD=
-export CONFIG_JDBC_URL=jdbc:postgresql://127.0.0.1/config
-
-java -Xmx4g -jar -DINSTANCE_NAME=TEST /opt/sftpreader/sftpreader-1.0-SNAPSHOT.jar
