2e20f203cd3921ee30150df37d14cb04b9e62334
Drew Littler <drewlittler@hotmail.com>
2019-11-19 14:50:11 +0000

    Change to Vision, how it gets ODS code

--- src/sftpreader/src/main/java/org/endeavourhealth/sftpreader/implementations/vision/VisionBatchSplitter.java
+++ src/sftpreader/src/main/java/org/endeavourhealth/sftpreader/implementations/vision/VisionBatchSplitter.java
@@ -3,10 +3,8 @@ package org.endeavourhealth.sftpreader.implementations.vision;
 import org.endeavourhealth.sftpreader.model.DataLayerI;
 
 import org.endeavourhealth.sftpreader.implementations.SftpBatchSplitter;
-import org.endeavourhealth.sftpreader.model.db.Batch;
-import org.endeavourhealth.sftpreader.model.db.BatchSplit;
-import org.endeavourhealth.sftpreader.model.db.DbConfiguration;
-import org.endeavourhealth.sftpreader.model.db.DbInstanceEds;
+import org.endeavourhealth.sftpreader.model.db.*;
+import org.endeavourhealth.sftpreader.model.exceptions.SftpFilenameParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -29,10 +27,20 @@ public class VisionBatchSplitter extends SftpBatchSplitter {
         batchSplit.setBatchId(batch.getBatchId());
         batchSplit.setLocalRelativePath(batch.getLocalRelativePath());
 
+        //using the remote path to find the ODS code doesn't work on Windows machines, as they have a different path separator,
+        //so pull the ODS code out of the file names instead
+        List<BatchFile> files = batch.getBatchFiles();
+        BatchFile firstFile = files.get(0);
+        String fileName = firstFile.getFilename();
+        String[] majorParts = fileName.split("-", 3);
+        String[] minorParts = majorParts[0].split("_");
+        String odsCode = minorParts[2];
+        batchSplit.setOrganisationId(odsCode);
+
         //for Vision SFTP, the last part o the remote path is the practice code which is used for the batch split
-        int lastPathPartIndex = dbConfiguration.getSftpConfiguration().getRemotePath().lastIndexOf('/');
+        /*int lastPathPartIndex = dbConfiguration.getSftpConfiguration().getRemotePath().lastIndexOf('/');
         String orgCode = dbConfiguration.getSftpConfiguration().getRemotePath().substring(lastPathPartIndex+1);
-        batchSplit.setOrganisationId(orgCode);
+        batchSplit.setOrganisationId(orgCode);*/
 
         ret.add(batchSplit);
 
