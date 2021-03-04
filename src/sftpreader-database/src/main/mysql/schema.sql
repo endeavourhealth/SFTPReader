cbaabd5963d6c48dc77f71b57bc0eae6497e93a4
Leigh Ellwood <leigh.ellwood@ntlworld.com>
2021-03-04 16:03:18 +0000

    Database script typos corrected

--- src/sftpreader-database/src/main/mysql/schema.sql
+++ src/sftpreader-database/src/main/mysql/schema.sql
@@ -1,4 +1,4 @@
-CRETE DATABASE sftp_reader;
+CREATE DATABASE sftp_reader;
 USE sftp_reader;
 
 DROP TABLE IF EXISTS configuration_paused_notifying;
@@ -172,7 +172,7 @@ CREATE TABLE batch
   is_complete boolean NOT NULL,
   complete_date datetime null,
   extract_date datetime null,
-  extract_cutoff datetime null;
+  extract_cutoff datetime null,
   CONSTRAINT batch_filesetid_pk PRIMARY KEY (batch_id),
   CONSTRAINT batch_configurationid_interfacetypeid_fk FOREIGN KEY (configuration_id, interface_type_id)
       REFERENCES configuration (configuration_id, interface_type_id) MATCH SIMPLE
